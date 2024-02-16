bdd29e1ed80c8087ac13db3857a8d202727c6c3c
Waxenov <waxenov@mail.ru>
2024-02-16 18:09:18 +0600

    установлен laravel jetstream

--- OSPanel/domains/laravel/routes/web.php
+++ OSPanel/domains/laravel/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/counter', Counter::class);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
